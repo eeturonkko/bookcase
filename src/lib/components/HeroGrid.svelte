f9f73fc7c35e8cd502be2276233b52ace2833064
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-03-30 12:03:55 +0200

    Add: herosection

---
+++ src/lib/components/HeroGrid.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+
+	const cardData = [
+		{
+			title: 'Discover',
+			desc: 'Find and review books online',
+			content:
+				'Discover books from a wide range of categories and authors. Find your next read and share your thoughts with the community.'
+		},
+		{
+			title: 'Contribute',
+			desc: 'Share your thoughts and reviews with the community',
+			content:
+				'Share your thoughts and reviews with the community. Contribute to the database by adding books and authors.'
+		},
+		{
+			title: 'Database',
+			desc: 'Explore a diverse database of books and authors.',
+			content:
+				'Explore a diverse database of books and authors. Find books from a wide range of categories and authors.'
+		},
+		{
+			title: 'Organize',
+			desc: 'Organize your reading list',
+			content:
+				"Organize your reading list. Keep track of books you've read, want to read, and are currently reading."
+		}
+	];
+</script>
+
+<div class="mt-6 grid grid-cols-2 place-items-center gap-5 p-4">
+	{#each cardData as { title, desc, content }}
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>{title}</Card.Title>
+				<Card.Description>{desc}</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<p class="text-balance">{content}</p>
+			</Card.Content>
+		</Card.Root>
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import HeroGrid from '$lib/components/HeroGrid.svelte';
 </script>
 
-<section class="flex flex-col">
-	<h1 class="text-4xl font-bold">Page</h1>
-	<p class="mt-4">This is a page.</p>
+<section class="mt-6 flex flex-col text-center tracking-wide">
+	<div>
+		<h2 class="text-3xl font-semibold">Welcome to the Bookcase</h2>
+		<p class="text-gray-700">We offer a diverse database of books and authors</p>
+		<p class="text-gray-700">Find and review books online</p>
+	</div>
+	<HeroGrid />
 </section>
